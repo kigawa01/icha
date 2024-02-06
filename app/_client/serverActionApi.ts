102e929f164f6bd473cc8881ad22c9caadf8fbc0
kigawa <kigawa.8390@gmail.com>
2024-02-07 00:08:57 +0900

    add: user manager

--- app/_client/serverActionApi.ts
+++ app/_client/serverActionApi.ts
@@ -1,31 +1,31 @@
 "use server";
 import {api} from "./api";
-import {AxiosError} from "axios";
+import {AxiosError, AxiosResponse} from "axios";
 import {ErrorRes} from "../../script/response/response";
-import {PostUserRes, TokensRes} from "../../api_clients";
+import {PostUserRes, TokensRes, UserRes} from "../../api_clients";
 
 export async function createUser(email: string, password: string, username: string): Promise<Result<PostUserRes>> {
-  return await api.defaultApi.createUserApiUserPost({
+  return await fetch(api.defaultApi.createUserApiUserPost({
     email: email, name: username, password: password,
-  }).then(value => {
-    return {value: value.data};
-  }).catch((reason: AxiosError<ErrorRes>) => {
-    return {error: reason.response?.data};
-  });
+  }));
 }
 
 export async function login(email: string, password: string): Promise<Result<TokensRes>> {
-  return await api.defaultApi.loginApiLoginPost({
+  return await fetch(api.defaultApi.loginApiLoginPost({
     email: email, password: password,
-  }).then(value => {
-    return {value: value.data};
-  }).catch((reason: AxiosError<ErrorRes>) => {
-    return {error: reason.response?.data};
-  });
+  }));
 }
 
 export async function refresh(): Promise<Result<TokensRes>> {
-  return await api.defaultApi.refreshTokenApiLoginRefreshPost().then(value => {
+  return await fetch(api.defaultApi.refreshTokenApiLoginRefreshPost());
+}
+
+export async function getSelfUser(): Promise<Result<UserRes>> {
+  return await fetch(api.defaultApi.getSelfUserApiUserSelfGet());
+}
+
+async function fetch<R>(res: Promise<AxiosResponse<R>>): Promise<Result<R>> {
+  return res.then(value => {
     return {value: value.data};
   }).catch((reason: AxiosError<ErrorRes>) => {
     return {error: reason.response?.data};
--- app/_manager/userManager.ts
+++ app/_manager/userManager.ts
@@ -1,6 +1,7 @@
-"use client";
 import {useMemo} from "react";
 import {useLoginState} from "./loginManager";
+import {GlobalState} from "../../script/util/hook/globalState";
+import {getSelfUser} from "../_client/serverActionApi";
 
 export interface User {
   name: string;
@@ -8,10 +9,56 @@ export interface User {
   uid: number;
 }
 
-export function useUserState() {
+const readyState = new GlobalState(false);
+const userState = new GlobalState<User | undefined>(undefined);
+
+export class UserManager {
+
+  constructor() {
+    getSelfUser().then(value => {
+      if (value.value) {
+        userState.set({email: value.value.email, name: value.value.name, uid: value.value.uid});
+        return;
+      }
+      if (value.error) console.error(value.error);
+      readyState.set(true);
+    }).catch(reason => {
+      console.error(reason);
+      readyState.set(true);
+    });
+  }
+
+}
+
+export interface UserState {
+  userManager: UserManager;
+  user: User | undefined;
+  ready: boolean;
+}
+
+let usermanager: UserManager | undefined = undefined;
+
+export function useUserState(): UserState | undefined {
   const loginState = useLoginState();
+  const user = userState.use();
+  const ready = readyState.use();
+
+  const userManager = useMemo(() => {
+    if (loginState == undefined) return undefined;
+    if (!loginState.ready) return undefined;
+    if (usermanager != undefined) return usermanager;
+    usermanager = new UserManager();
+
+    return usermanager;
+  }, [loginState]);
 
   return useMemo(() => {
-    if (loginState == undefined) return undefined;
-  }, []);
+    if (userManager == undefined) return undefined;
+
+    return {
+      ready: ready,
+      userManager: userManager,
+      user: user,
+    };
+  }, [userManager, user]);
 }
