9889c7f5be615f575bc94abac76c153540035357
kigawa <kigawa.8390@gmail.com>
2024-02-22 03:03:51 +0900

    add: gacha run

--- app/gacha/[id]/result/page.tsx
+++ app/gacha/[id]/result/page.tsx
@@ -1,17 +1,19 @@
-import {Box} from "@mui/system";
 import {GachaTopImage} from "../../GachaTopImage";
 import {Button, Typography} from "@mui/material";
 import {TextSection} from "../../../_unit/_section/TextSection";
+import {redirect} from "next/navigation";
+import {Main} from "../../../_unit/Main";
 
 export default function Page(
-  {}: {},
+  {params}: { params: { id: string } },
 ) {
+  const uid = parseInt(params.id);
+  if (isNaN(uid)) redirect("/notfound");
 
-
-  return <Box>
+  return <Main>
     <Typography variant={"h2"}>ガチャ結果</Typography>
     <GachaTopImage/>
     <TextSection content={"ガチャ名"} sectionTitle={"説明"}/>
-    <Button>もう一度引く</Button>
-  </Box>;
+    <Button variant={"contained"}>もう一度引く</Button>
+  </Main>;
 }
--- app/gacha/[id]/run/page.tsx
+++ app/gacha/[id]/run/page.tsx
@@ -1,15 +1,24 @@
+"use client";
 import {Main} from "../../../_unit/Main";
 import {Button, Typography} from "@mui/material";
+import {redirect, useRouter} from "next/navigation";
+import {RequireLogin} from "../../../_unit/RedirectLogin";
 
 export default function Page(
-  {}: {},
+  {params}: { params: { id: string } },
 ) {
+  const uid = parseInt(params.id);
+  if (isNaN(uid)) redirect("/notfound");
+  const router = useRouter();
 
   return (
     <Main>
+      <RequireLogin/>
       <Typography variant={"h2"}>ガチャ名</Typography>
-      <Button>止める</Button>
-      <Button>Skip</Button>
+      <Button variant={"contained"}>止める</Button>
+      <Button variant={"outlined"} sx={{color: "text.primary"}} onClick={_ => {
+        router.push("result");
+      }}>Skip</Button>
     </Main>
   );
 }
