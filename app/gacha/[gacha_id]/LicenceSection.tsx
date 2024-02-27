e1c6b7f343ffe717769aafffe818b7726d6f922f
kigawa <kigawa.8390@gmail.com>
2024-02-28 01:19:45 +0900

    add: licence section style

--- app/gacha/[gacha_id]/LicenceSection.tsx
+++ app/gacha/[gacha_id]/LicenceSection.tsx
@@ -2,8 +2,8 @@ import {BoxTypeMap} from "@mui/system/Box/Box";
 import {OverrideProps} from "@mui/types";
 import {Section} from "../../_unit/_section/Section";
 import {LicenceData} from "../../../api_clients";
-import {Typography} from "@mui/material";
 import {LabeledText} from "../../_unit/_labeled/LabeledText";
+import {Box} from "@mui/system";
 
 export function LicenceSection(
   {
@@ -18,12 +18,30 @@ export function LicenceSection(
       {...props}
       sectionTitle={"ライセンス"}
     >
-      <Typography>{licence?.text}</Typography>
-      <LabeledText text={licence?.business} label={"商用利用"}/>
-      <LabeledText text={licence?.post} label={"投稿"}/>
-      <LabeledText text={licence?.credit} label={"クレジット表記なし"}/>
-      <LabeledText text={licence?.distribution} label={"二次配布"}/>
-      <LabeledText text={licence?.material} label={"素材"}/>
+      <Box
+        padding={"10px"} component={"pre"} borderRadius={"5px"} boxShadow={1} bgcolor={"lightgrey"}
+        border={"1px solid grey"} margin={"10px 0 30px 0"}
+      >{licence?.text}</Box>
+      <LabeledText
+        labelProps={{width: "200px"}} borderBottom={"1px solid grey"} padding={"3px 10px"} margin={"10px 0"}
+        text={licence?.business} label={"商用利用"}
+      />
+      <LabeledText
+        labelProps={{width: "200px"}} borderBottom={"1px solid grey"} padding={"3px 10px"} margin={"10px 0"}
+        text={licence?.post} label={"投稿"}
+      />
+      <LabeledText
+        labelProps={{width: "200px"}} borderBottom={"1px solid grey"} padding={"3px 10px"} margin={"10px 0"}
+        text={licence?.credit} label={"クレジット表記なし"}
+      />
+      <LabeledText
+        labelProps={{width: "200px"}} borderBottom={"1px solid grey"} padding={"3px 10px"} margin={"10px 0"}
+        text={licence?.distribution} label={"二次配布"}
+      />
+      <LabeledText
+        labelProps={{width: "200px"}} borderBottom={"1px solid grey"} padding={"3px 10px"} margin={"10px 0"}
+        text={licence?.material} label={"素材"}
+      />
     </Section>
   );
 }
--- app/gacha/[gacha_id]/page.tsx
+++ app/gacha/[gacha_id]/page.tsx
@@ -12,6 +12,8 @@ import {useUserState} from "../../_manager/UserProvider";
 import {redirectLoginRouter} from "../../_unit/RedirectLogin";
 import {LoadableButton} from "../../_unit/_loading/LoadableButton";
 import {useClientState} from "../../_manager/AuthApiProvider";
+import {Box} from "@mui/system";
+import {Button, Typography} from "@mui/material";
 
 export default function Page(
   {params}: { params: { gacha_id: string } },
@@ -30,17 +32,29 @@ export default function Page(
   const gacha = gachaRes?.result;
 
   return <Main>
-    <Img src={gacha?.thumbnail.base64} alt={gacha?.thumbnail.name} width={"100%"}/>
-    <TextSection content={gacha?.name || ""} sectionTitle={"説明"}/>
-    <LoadableButton loading={userState == undefined} variant={"contained"} onClick={() => {
-      if (userState == undefined) return;
-      if (userState.userRes == undefined) redirectLoginRouter(router, `/gacha/${uid}/run`);
-      else router.push(`/gacha/${uid}/run`);
-    }} disabled={userState == undefined}
-    >
-      {userState?.userRes ? "ガチャを引く" : "ログインしてガチャを引く"}
-    </LoadableButton>
+    <Typography variant={"h2"} margin={"10px"}>{gacha?.name}</Typography>
+    <Img
+      src={gacha?.thumbnail.base64} alt={gacha?.thumbnail.name} width={"100%"} height={"500px"} borderRadius={"5px"}
+      boxShadow={1} padding={"3px"}
+    />
+    <TextSection content={gacha?.name || ""} sectionTitle={gacha?.name || ""}/>
+    <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+      <Typography sx={{fontSize: "4.5rem"}}>&gt;&gt;</Typography>
+      <LoadableButton
+        loading={userState == undefined} variant={"contained"} onClick={() => {
+        if (userState == undefined) return;
+        if (userState.userRes == undefined) redirectLoginRouter(router, `/gacha/${uid}/run`);
+        else router.push(`/gacha/${uid}/run`);
+      }} disabled={userState == undefined} sx={{fontSize: "2rem", height: "fit-content", margin: "0 10px"}}
+      >
+        {userState?.userRes ? "ガチャを引く" : "ログインしてガチャを引く"}
+      </LoadableButton>
+      <Typography sx={{fontSize: "4.5rem"}}>&lt;&lt;</Typography>
+    </Box>
     <LicenceSection licence={gacha?.licence}/>
     <GachaContents gachaId={gacha?.uid || 0} contents={gacha?.contents || []}/>
+    <Box display={"flex"} justifyContent={"right"} margin={"30px 0 0 0"}>
+      <Button variant={"contained"} onClick={_ => router.back()}>前に戻る</Button>
+    </Box>
   </Main>;
 }
