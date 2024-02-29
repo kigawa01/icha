25f7bccf85ff169034caf41062fa758dd9295bc3
kigawa <kigawa.8390@gmail.com>
2024-03-01 03:11:53 +0900

    add: gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import {Footer} from "./_footer/Footer";
 import {Toolbar} from "@mui/material";
 import {Box} from "@mui/system";
 import UserProvider from "./_manager/UserProvider";
+import Script from "next/script";
 
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
 export default function RootLayout(
   {
     children,
@@ -20,17 +22,22 @@ export default function RootLayout(
       <meta content="width=device-width, initial-scale=1" name="viewport"/>
       <title>Icha</title>
       {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-E2QSM2RBMZ"></script>
-      <script>
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-E2QSM2RBMZ');        
-        `}
-      </script>
-
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', '${GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
     </head>
     <Box
       component={"body"}
